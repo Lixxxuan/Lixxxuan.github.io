5c57637b0c66a9e1a6cea29ebfb1b6a1175e1d1c
Lixxxuan <1366070554@qq.com>
2025-08-01 13:16:23 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Lixxxuan',
-  bio: 'text?',
+  bio: 'Lixxxuan\'s personal site',
   links: [
     {
       name: 'Twitter',
